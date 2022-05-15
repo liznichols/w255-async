66b177fc92edb1915e0ac18dcf618691b2ceb90c
liznichols <liz.nichols@berkeley.edu>
2022-05-15 00:01:04 -0500

    update tests

--- w255_flask_ci_demo/resources/default.py
+++ w255_flask_ci_demo/resources/default.py
@@ -5,5 +5,8 @@ from api.app import api
 class DefaultResource(Resource):
 	def get(self):
 		return {"status": "success", "data": {"msg": "Hello World"}}
+        
+    def post(self):
+        return {"status": "success", "data": {"msg": "Hello World"}}
 
 api.add_resource(DefaultResource, "/", endpoint="home")
---
+++ w255_flask_ci_demo/tests/test_root.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest import TestCase
+
+from api.app import create_app
+
+class UserTestCase(TestCase):
+	def setUp(self):
+		self.app = create_app("testing") #instantiate application
+		self.app_context = self.app.app_context()
+		self.app_context.push()
+		db.create_all()
+		self.client = self.app.test_client(use_cookies=True)
+		
+	def tearDown(self):
+		db.session.remove()
+		db.drop_all()
+		self.app_context.pop()
+
+	def test_root_response(self):
+        '''note the post vs get method, both must be defined in resources/default.py'''
+		response = self.client.get("/", headers={"Content-Type": "application/json"},)
+
+		self.assertEqual(response.status_code,200)
